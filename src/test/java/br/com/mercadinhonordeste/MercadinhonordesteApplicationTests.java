9d177550779dbacd457d8f3a120aa24d97c86980
Marcos Dinei <marcos.fontes@parnamirim.rn.gov.br>
2023-02-16 13:12:25 -0300

    inital commit

---
+++ src/test/java/br/com/mercadinhonordeste/MercadinhonordesteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mercadinhonordeste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadinhonordesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
