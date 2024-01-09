6809c5d16e199640e4f7eaffbc95f15e5abe6e60
Marcos Dinei <marcos.fontes@parnamirim.rn.gov.br>
2024-01-09 11:17:33 -0300

    feat: starting unit tests

---
+++ src/test/java/br/com/mercadinhonordeste/service/ProductServiceTest.java
@@ -0,0 +1,122 @@
+package br.com.mercadinhonordeste.service;
+
+import br.com.mercadinhonordeste.entity.Product;
+import br.com.mercadinhonordeste.entity.ProductBox;
+import br.com.mercadinhonordeste.entity.Stock;
+import br.com.mercadinhonordeste.exception.NotFoundExeption;
+import br.com.mercadinhonordeste.model.Item;
+import br.com.mercadinhonordeste.model.PaginatedData;
+import br.com.mercadinhonordeste.repository.ProductBoxRepository;
+import br.com.mercadinhonordeste.repository.ProductRepository;
+import br.com.mercadinhonordeste.repository.StockRepository;
+import br.com.mercadinhonordeste.service.criteria.ProductCriteria;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.*;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+
+@ExtendWith(MockitoExtension.class)
+class ProductServiceTest {
+    @InjectMocks
+    private ProductService service;
+    @Mock
+    private ProductRepository repository;
+    @Mock
+    private StockRepository stockRepository;
+    @Mock
+    private ProductBoxRepository boxRepository;
+    @Mock
+    private Product product;
+    @Mock
+    private Page<Product> products;
+    @Mock
+    private ProductCriteria criteria;
+    @Mock
+    private Specification<Product> specification;
+    @Mock
+    private ProductBox box;
+    @Captor
+    private ArgumentCaptor<Stock> stockCaptor;
+
+    @Test
+    void shouldSaveNewProductAndNewStock() {
+        given(repository.save(product)).willReturn(product);
+
+        service.saveProduct(product);
+
+        then(repository).should().save(product);
+        then(stockRepository).should().save(stockCaptor.capture());
+        assertEquals(product, stockCaptor.getValue().getProduct());
+    }
+
+    @Test
+    void shouldUpdateProductAndBox() {
+        given(repository.existsById(product.getId())).willReturn(true);
+        given(boxRepository.findByProductId(product.getId())).willReturn(Optional.of(box));
+
+        service.updateProduct(product);
+
+        then(boxRepository).should().delete(box);
+        then(repository).should().save(product);
+    }
+
+    @Test
+    void shouldNotFindProductByIdOnUpdate() {
+        given(repository.existsById(product.getId())).willReturn(false);
+
+        assertThrows(NotFoundExeption.class, () -> service.updateProduct(product));
+        then(repository).should().existsById(product.getId());
+    }
+
+    @Test
+    void shouldReturnProductByCode() {
+        given(repository.findByCode(Mockito.anyString())).willReturn(Optional.of(product));
+
+        Item item = service.getProductByCode(Mockito.anyString());
+
+        then(repository).should().findByCode(Mockito.anyString());
+        assertEquals(item.getProduct(), product);
+        assertFalse(item.getBox());
+    }
+
+    @Test
+    void shouldReturnProductBoxByCode() {
+        given(repository.findByCode(Mockito.anyString())).willReturn(Optional.empty());
+        given(repository.findByBoxCode(Mockito.anyString())).willReturn(Optional.of(product));
+
+        Item item = service.getProductByCode(Mockito.anyString());
+
+        then(repository).should().findByCode(Mockito.anyString());
+        assertEquals(item.getProduct(), product);
+        assertTrue(item.getBox());
+    }
+
+    @Test
+    void shouldNotFindProductByCode() {
+        given(repository.findByCode(Mockito.anyString())).willReturn(Optional.empty());
+        given(repository.findByBoxCode(Mockito.anyString())).willReturn(Optional.empty());
+
+        assertThrows(NotFoundExeption.class, () -> service.getProductByCode(Mockito.anyString()));
+        then(repository).should().findByCode(Mockito.anyString());
+    }
+
+    @Test
+    void shouldListProducts() {
+        Pageable pageable = PageRequest.of(1, 10);
+
+        PaginatedData<Product> response = service.listProducts(criteria, pageable);
+
+        then(repository).should().findAll(specification, pageable);
+        assertEquals(response.getContent(), products.getContent());
+    }
+}
