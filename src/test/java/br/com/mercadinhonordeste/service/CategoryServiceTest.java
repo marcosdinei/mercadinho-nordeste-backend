5f765647cb92f0b7b5f83b1bdf2ff85ea8d5b1d3
Marcos Dinei <marcos.fontes@parnamirim.rn.gov.br>
2024-01-11 08:37:53 -0300

    feat: category service tests

---
+++ src/test/java/br/com/mercadinhonordeste/service/CategoryServiceTest.java
@@ -0,0 +1,48 @@
+package br.com.mercadinhonordeste.service;
+
+import br.com.mercadinhonordeste.entity.Category;
+import br.com.mercadinhonordeste.repository.CategoryRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Sort;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+
+@ExtendWith(MockitoExtension.class)
+class CategoryServiceTest {
+    @InjectMocks
+    private CategoryService service;
+    @Mock
+    private CategoryRepository repository;
+    @Mock
+    private Category category;
+    @Mock
+    private List<Category> categories;
+
+    @Test
+    void shouldSaveNewCategory() {
+        given(repository.save(category)).willReturn(category);
+
+        service.saveCategory(category);
+
+        then(repository).should().save(category);
+    }
+
+    @Test
+    void shouldListCategoriesByName() {
+        String name = "abc";
+        given(repository.findByNameContainingIgnoreCase(Mockito.anyString(), Mockito.any())).willReturn(categories);
+
+        service.listCategories(name);
+
+        then(repository).should().findByNameContainingIgnoreCase(Mockito.anyString(), Mockito.any());
+    }
+}
